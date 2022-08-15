71530e5ebe5df1203a2c258f673bbf6b056310f8
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-08-15 14:21:01 +0530

    Update start.py

--- EndString/start.py
+++ EndString/start.py
@@ -2,10 +2,11 @@ from . import Client, filters, Message, start_markup, IKM
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(_, m):
-    await m.reply("""Welcome to String Session Generator.
+    ok = await m.reply("""Welcome to String Session Generator.
 
 
 You can procees with bot's api values if you want , else you can proceed with your api values
 
 
 Press Button to start generating session!""", reply_markup=IKM(start_markup))
+    m_id = ok.message_id
