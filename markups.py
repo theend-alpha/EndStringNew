201e947dad599b201648c9bd6a69bba8ffaccb3a
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-08-15 14:17:35 +0530

    Create markups.py

---
+++ markups.py
@@ -0,0 +1,6 @@
+from EndString import IKB
+
+start_markup = [
+                [IKB(" 🔗 Generate string with your API", data="OWN_API"),
+                IKB(" 🔗 Generate string with bot API", data="BOT_API")
+                ]
